d81c10ac328ba29d28866cf6c7217e4670128b5b
Kushagra1312 <gkushagra48@gmail.com>
2023-04-26 17:13:05 +0530

    removed send grid configs

--- server/helper/mailer.js
+++ server/helper/mailer.js
@@ -1,6 +1,4 @@
-const sgMail = require('@sendgrid/mail');
 require("dotenv").config();
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const nodemailer = require("nodemailer")
 
 let transporter = nodemailer.createTransport({
